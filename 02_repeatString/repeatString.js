ef82ad4240d9663ab277c078f6eee5149111d57c
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-05-26 20:00:54 -0700

    Repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = '';
+
+    if(num < 0) return result = 'ERROR';
+
+    for(let i = 0; i < num; i++){
+        result += string;
+    }
+    return result;
 
 };
 
