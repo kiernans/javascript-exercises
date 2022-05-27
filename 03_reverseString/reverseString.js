193fb8441dc8b1f015be58d92debed157686ef46
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-05-26 21:29:55 -0700

    Reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = '';
+    let length = string.length;
 
+    for(let i = 0; i < length; i++) {
+        result += string.charAt(string.length - 1);
+        string = string.slice(0, -1);
+    }
+    return result;
 };
 
 // Do not edit below this line
