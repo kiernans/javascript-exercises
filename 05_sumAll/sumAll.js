e2f8cda8a8eb751e43c949087e6bd9c7715d811c
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-05-30 10:40:03 -0700

    SumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+    let smaller;
+    let bigger;
 
+    if(!(typeof(num2) === "number") 
+        || ((num1 < 0) || num2 < 0)) return 'ERROR';
+
+    if(num1 > num2) {
+        smaller = num2;
+        bigger = num1;
+    } else {
+        smaller = num1;
+        bigger = num2;
+    }
+
+    let result = smaller;
+
+    for (let i = smaller+1; i <= bigger; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
