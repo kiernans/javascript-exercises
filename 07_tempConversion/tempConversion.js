1e2703ef68d0a34cd21989b561e0a9b4e6ec26a4
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-05-30 11:29:54 -0700

    TempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return parseFloat(
+    Math.round(((temp - 32) * (5/9)) * 10) / 10
+    .toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return parseFloat(
+    Math.round((temp * (9/5) + 32) * 10) / 10
+    .toFixed(1));
 };
 
 // Do not edit below this line
