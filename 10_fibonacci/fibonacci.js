3fbbeacfc50c6b9749a932f94a1f00021f5f5cf8
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-02 12:37:08 -0700

    Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = (x) => {
+    if (x < 0) return 'OOPS';
+    if(x <= 1) return parseInt(x);
+    return fibonacci(x-1) + fibonacci(x-2);
 };
 
 // Do not edit below this line
