3e6b6004a86d5340243faf810cbb617529e587f1
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-02 10:37:36 -0700

    Calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = (array) => {
+  return array.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = (array) => {
+  return array.reduce((total, item) => {
+    return total * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = (a,b) =>  {
+  let total = 1;
+  for(let i = 0; i < b; i++){
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
