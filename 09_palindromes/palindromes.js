04eb5b8909cc31ad26285eea5973ba188afc6206
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-02 12:20:47 -0700

    Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,39 @@
-const palindromes = function () {
+const palindromes = (string) => {
+    if(string.length === 0) return true;
 
+    removePunctuation(string);
+
+    if(string.charAt[0] === string.charAt[-1]) {
+        return palindromes(string.slice(1,-2));
+    }
 };
+
+const removePunctuation = (string) => {
+    for(let i = 0; i < string.length; i++) {
+        switch(string.charAt(i)){
+            case '!':
+                string = string.replace('!', '');
+                i--;
+                break;
+            case '?':
+                string = string.replace('?', '');
+                i--;
+                break;
+            case '.':
+                string = string.replace('.', '');
+                i--;
+                break;
+            case ',':
+                string = string.replace(',', '');
+                i--;
+                break;
+            default:
+        }
+    }
+    return string;
+};
+
+console.log(removePunctuation('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
